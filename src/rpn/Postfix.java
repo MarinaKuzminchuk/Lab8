91588273431c1675bafede3a82858645f5c2f33f
S0568243 <82872622+S0568243@users.noreply.github.com>
2021-06-05 17:08:06 +0200

    Update Postfix.java

--- src/rpn/Postfix.java
+++ src/rpn/Postfix.java
@@ -1,5 +1,35 @@
 package rpn;
 
-public class Postfix {
+import java.util.Scanner;
+
+public abstract class Postfix{
+	
+	int res = 0;
+	
+	int x = 0;
+	int y = 0;
+	public int evaluate (String pfx) {
+		
+		 Scanner sc = new Scanner(pfx);
+		 	StackAsList<Integer> stack = new StackAsList<Integer>();
+
+		    while (sc.hasNext()) {
+		        if (sc.hasNextInt()) {
+		            stack.push(sc.nextInt());
+		            continue;
+		        }
+		        int b = stack.pop();
+		        int a = stack.pop();
+		        char op = sc.next().charAt(0);
+		        if      (op == '+') stack.push(a + b);
+		        else if (op == '-') stack.push(a - b);
+		        else if (op == '*') stack.push(a * b);
+		        else if (op == '/') stack.push(a / b);
+		        else if (op == '%') stack.push(a % b);
+		    }
+
+		    sc.close();
+		    return stack.pop();
+	}
 
 }
