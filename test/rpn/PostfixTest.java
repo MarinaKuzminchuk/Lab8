821f13f1fc5f2b6936ced5dc1aeac70e0eecaba6
S0568243 <82872622+S0568243@users.noreply.github.com>
2021-06-08 22:27:25 +0200

    Update PostfixTest.java

--- test/rpn/PostfixTest.java
+++ test/rpn/PostfixTest.java
@@ -8,9 +8,17 @@ public class PostfixTest {
 
     @Test
     public void evaluate() {
+        double delta = 0.001;
+
         Postfix postfix = new Postfix();
-        assertEquals(16, postfix.evaluate("8 2 / 2 2 + *"));
-        assertEquals(26, postfix.evaluate("4 5 6 * + 8 -"));
-        assertEquals(536, postfix.evaluate("18 3 / 2 ^ 13 7 + 5 2 ^ * +"));
+        assertEquals(5, postfix.evaluate("1 2 * 3 +"), delta);
+        assertEquals(7, postfix.evaluate("1 2 3 * +"), delta);
+        assertEquals(-78, postfix.evaluate("1 2 + 3 4 ^ -"), delta);
+        assertEquals(-11, postfix.evaluate("1 2 ^ 3 4 * -"), delta);
+
+        assertEquals(-1011, postfix.evaluate("1 2 3 * + 4 5 ^ - 6 +"), delta);
+        assertEquals(9.25, postfix.evaluate("1 2 + 3 * 4 5 6 - ^ +"), delta);
+        assertEquals(98.75, postfix.evaluate("1 2 + 3 4 / + 5 + 6 7 8 + * +"), delta);
+        assertEquals(-1, postfix.evaluate("9 1 - 2 - 3 2 * - 1 -"), delta);
     }
 }
