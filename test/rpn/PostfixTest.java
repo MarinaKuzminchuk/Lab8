e29035528647347b6822d63affa56d4ccc5cd504
S0568243 <82872622+S0568243@users.noreply.github.com>
2021-06-08 21:19:01 +0200

    Add files via upload
    Post fix test für evaluate

--- test/rpn/PostfixTest.java
+++ test/rpn/PostfixTest.java
@@ -1,21 +1,16 @@
-package rpn;
+package lab8;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class PostfixTest {
+public class PostfixTest {
 
     @Test
-    public void infixToPostfix() {
+    public void evaluate() {
         Postfix postfix = new Postfix();
-        assertEquals("1 2 * 3 +", postfix.infixToPostfix("1 * 2 + 3"));
-        assertEquals("1 2 3 * +", postfix.infixToPostfix("1 + 2 * 3"));
-        assertEquals("1 2 + 3 4 ^ -", postfix.infixToPostfix("1 + 2 - 3 ^ 4"));
-        assertEquals("1 2 ^ 3 4 * -", postfix.infixToPostfix("1 ^ 2 - 3 * 4"));
-        assertEquals("1 2 3 * + 4 5 ^ - 6 +", postfix.infixToPostfix("1 + 2 * 3 - 4 ^ 5 + 6"));
-        assertEquals("1 2 + 3 * 4 5 6 - ^ +", postfix.infixToPostfix("( 1 + 2 ) * 3 + ( 4 ^ ( 5 - 6 ) )"));
-        assertEquals("1 2 + 3 4 / + 5 + 6 7 8 + * +", postfix.infixToPostfix("1 + 2 + 3 / 4 + 5 + 6 * ( 7 + 8 )"));
-        assertEquals("9 1 - 2 - 3 2 * - 1 -", postfix.infixToPostfix("9 - 1 - 2 - 3 * 2 - 1"));
+        assertEquals(16, postfix.evaluate("8 2 / 2 2 + *"));
+        assertEquals(26, postfix.evaluate("4 5 6 * + 8 -"));
+        assertEquals(536, postfix.evaluate("18 3 / 2 ^ 13 7 + 5 2 ^ * +"));
     }
 }
